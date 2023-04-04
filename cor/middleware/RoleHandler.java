8d82713eadbbbf25b17ea51428346640916709de
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-04-04 18:51:40 +0700

    Add Handler for RoleHandler

--- cor/middleware/RoleHandler.java
+++ cor/middleware/RoleHandler.java
@@ -4,12 +4,16 @@ import framework.Handler;
 import framework.Request;
 
 public class RoleHandler implements Handler {
+    private Handler nextHandler;
 
     @Override
     public void handle(Request request) {
 
         // TODO Auto-generated method stub
 
+        if (request.getUser().getRole().equals("user")) {
+            this.nextHandler.handle(request);
+        }
     }
 
     public void setNextHandler(Handler handler) {
